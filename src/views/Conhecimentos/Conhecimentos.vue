15cbd1c49691d38b5408458261d9f8c6945faff9
Mateus Maranhão <feradourada@gmail.com>
2022-12-20 17:23:34 -0300

    Implements Conhecimentos.vue e Contatos.vue

--- src/views/Conhecimentos/Conhecimentos.vue
+++ src/views/Conhecimentos/Conhecimentos.vue
@@ -1,10 +1,86 @@
 <template>
     <div class="container">
       <div class="row">
-        <div class="col">
+        <div class="col-md-12">
             <div class="conhecimento">
                 <h1><span class="h1">&lt;h1&gt;</span>Conhecimentos na área de T.I<span class="h1">&lt;h1&gt;</span></h1>
+                <h3><span class="h3">&lt;h3&gt;</span>Área de atuação<span class="h3">&lt;h3&gt;</span></h3>
             </div>
+        </div>
+        <div class="col-md-6 conhecimentos">
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original.svg" 
+                /> 
+                VueJS
+            </h5>
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" 
+                /> 
+                JavaScript
+            </h5>
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" 
+                /> 
+                HTML5
+            </h5>
+        </div>
+        <div class="col-md-6 conhecimentos">
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" 
+                /> 
+                SCSS
+            </h5>
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png" 
+                /> 
+                Bootstrap
+            </h5>
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" 
+                /> 
+                CSS3
+            </h5>
+        </div>
+        <div class="col-md-12 conhecimento">
+            <h3><span class="h3">&lt;h3&gt;</span>Programas de uso<span class="h3">&lt;h3&gt;</span></h3>
+        </div>
+        <div class="col-md-3 conhecimento">
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png" 
+                /> 
+                VS Code (Editor de código)
+            </h5>
+        </div>
+        <div class="col-md-3 conhecimento">
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" 
+                /> 
+                GitHub (repositórios)
+            </h5>
+        </div>
+        <div class="col-md-3 conhecimento">
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Microsoft_Office_logo_%282019%E2%80%93present%29.svg/2048px-Microsoft_Office_logo_%282019%E2%80%93present%29.svg.png" 
+                /> 
+                Pacote Office
+            </h5>
+        </div>
+        <div class="col-md-3 conhecimento">
+            <h5>
+                <img class="img-fluid img-conhecimento" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/1051px-Adobe_Photoshop_CC_icon.svg.png" 
+                /> 
+                Photoshop (Para criar / editar fotos)
+            </h5>
         </div>
       </div>
     </div>
--- src/views/Contato/Contato.vue
+++ src/views/Contato/Contato.vue
@@ -1,3 +1,46 @@
 <template>
-    <h1>Contato</h1>
-</template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 contatos">
+                <h1><span class="h1">&lt;h1&gt;</span>Minhas redes sociais para contato<span class="h1">&lt;h1&gt;</span></h1>
+                <div class="d-grid gap-2">
+                    <button @click="getWhatsApp()" class="btn btn-success"><i class="fa-brands fa-whatsapp"></i> WhatsApp</button>
+                    <button @click="getInstagram()" class="btn btn-instagram"><i class="fa-brands fa-instagram"></i> Instagram</button>
+                    <button @click="getGitHub()" class="btn btn-dark"><i class="fa-brands fa-github"></i> GitHub</button>
+                    <button @click="getGmail()" class="btn btn-danger"><i class="fa-solid fa-envelope"></i> Gmail</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'Sobre',
+    methods: {
+        
+        getWhatsApp() {
+            window.open('https://wa.link/gvqccj')
+        },
+
+        getInstagram() {
+            window.open('https://www.instagram.com/mateus_maranhao12/')
+        },
+
+        getGitHub() {
+            window.open('https://github.com/mateusmaranhao12')
+        },
+
+        getGmail() {
+            window.open('https://mail.google.com/mail/feradourada@gmail.com')
+        },
+
+    }
+  }
+  </script>
+  
+  <style lang="scss">
+    @import 'Contato';
+  </style>
